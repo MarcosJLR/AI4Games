c9312b4ea9361db86033bd177164731a0446bf58
MarcosJLR <socram.693@gmail.com>
2020-01-19 05:59:15 -0400

    SDL Texture Wrapper class

---
+++ texture.cpp
@@ -0,0 +1,70 @@
+#include "texture.hpp"
+
+using namespace std;
+
+LTexture::LTexture()
+{
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+LTexture::~LTexture()
+{
+    free();
+}
+
+bool LTexture::loadFromFile(SDL_Renderer* renderer, string path)
+{
+    free();
+
+    SDL_Texture* newTexture = NULL;
+
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    
+    if (loadedSurface == NULL) 
+    {
+        fprintf(stderr, "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        return false;
+    }
+
+    SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0x00, 0xFF, 0xFF));
+
+    newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    if(newTexture == NULL)
+    {
+        fprintf(stderr, "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        return false;
+    }
+
+    mWidth = loadedSurface->w;
+    mHeight = loadedSurface->h;
+
+    SDL_FreeSurface(loadedSurface);
+
+    mTexture = newTexture;
+    return true;
+}
+
+void LTexture::free()
+{
+    if(mTexture != NULL){
+        SDL_DestroyTexture(mTexture);
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
+
+void LTexture::render(SDL_Renderer* renderer, int x, int y, SDL_Rect* clip, 
+                      double angle, SDL_Point* center, SDL_RendererFlip flip)
+{
+    SDL_Rect renderQuad = {x, y, mWidth, mHeight};
+
+    if(clip != NULL){
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
+
+    SDL_RenderCopyEx(renderer, mTexture, clip, &renderQuad, angle, center, flip);
+}                    
---
+++ texture.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+class LTexture
+{
+    public:
+        // Constructor
+        LTexture();
+
+        // Destructor
+        ~LTexture();
+
+        // Loads image
+        bool loadFromFile(SDL_Renderer* renderer, std::string path);
+
+        // Deallocates texture
+        void free();
+
+        void render(SDL_Renderer* renderer, int x, int y, 
+                    SDL_Rect* clip = NULL, double angle = 0.0, 
+                    SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+        inline int getWidth()
+        {
+            return mWidth;
+        }
+        inline int getHeight()
+        {
+            return mHeight;
+        }
+
+    private:
+        // The real texture
+        SDL_Texture* mTexture;
+
+        // Dimensions
+        int mWidth;
+        int mHeight;
+};
